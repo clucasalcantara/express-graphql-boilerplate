374a56603c5e06f8cc03e8ab3149d92cdd5a03d6
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-03-12 20:21:41 +0000

    Move console log to listen callback

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 
-app.listen(4000);
-
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
